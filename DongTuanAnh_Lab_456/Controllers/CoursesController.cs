1899ac3f3763bf28658c5aaef771bc4dc930eea8
Administrator <Administrator@PC250>
2020-06-05 15:11:36 +0700

    Update1

---
+++ DongTuanAnh_Lab_456/Controllers/CoursesController.cs
@@ -0,0 +1,30 @@
+﻿using DongTuanAnh_Lab_456.Models;
+using DongTuanAnh_Lab_456.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DongTuanAnh_Lab_456.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public CoursesController()
+        {
+            _dbContext = new ApplicationDbContext();
+        }
+
+        // GET: Courses
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel
+            {
+                Categories = _dbContext.Categories.ToList()
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ DongTuanAnh_Lab_456/Migrations/202006050710343_PopulateCategoryTable.cs
@@ -0,0 +1,19 @@
+namespace DongTuanAnh_Lab_456.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class PopulateCategoryTable : DbMigration
+    {
+        public override void Up()
+        {
+            Sql("Insert into Categories (ID, NAME) Values (1, 'Development')");
+            Sql("Insert into Categories (ID, NAME) Values (2, 'Business')");
+            Sql("Insert into Categories (ID, NAME) Values (3, 'Maarketing')");
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
