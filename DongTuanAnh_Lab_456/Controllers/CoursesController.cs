0a24952f4b1dd5fcbc3cb60ccd361f7fd54a9dbb
Administrator <Administrator@PC250>
2020-06-19 16:10:01 +0700

    GitHub 16:04

--- DongTuanAnh_Lab_456/Controllers/CoursesController.cs
+++ DongTuanAnh_Lab_456/Controllers/CoursesController.cs
@@ -74,5 +74,17 @@ namespace DongTuanAnh_Lab_456.Controllers
 
             return View(viewModel);
         }
+
+        [Authorize]
+        public ActionResult Mine()
+        {
+            var userId = User.Identity.GetUserId();
+            var courses = _dbContext.Courses
+                .Where(c => c.LecturerId == userId && c.DateTime > DateTime.Now)
+                .Include(l => l.Lecturer)
+                .Include(c => c.Category)
+                .ToList();
+            return View(courses);
+        }
     }
 }
